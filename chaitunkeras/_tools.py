7cfd57e1e3ae63a2e6660b0a114933e0e726be93
Santiago Parraguez <54079075+SanParraguez@users.noreply.github.com>
2021-03-20 20:20:46 -0300

    Add files via upload

--- chaitunkeras/_tools.py
+++ chaitunkeras/_tools.py
@@ -10,7 +10,10 @@ KERAS CHAITUN
   - TOOLS
 """
 # ============= IMPORTS ===============
+import random
 import numpy as np
+
+from itertools import product
 
 # =================================================================================
 def _check_param(values) -> object:
@@ -32,3 +35,88 @@ def _check_param(values) -> object:
         return values
     else:
         return [values]
+
+# =================================================================================
+class ParameterSampler(object):
+    # modified from scikit-learn and kerashypetune ParameterSampler
+    """
+    Generator on parameters sampled from given distributions.
+    Non-deterministic iterable over random candidate combinations for hyper-
+    parameter search. If all parameters are presented as a list,
+    sampling without replacement is performed. If at least one parameter
+    is given as a distribution, sampling with replacement is used.
+    It is highly recommended to use continuous distributions for continuous
+    parameters.
+
+    Parameters
+    ----------
+    param_distributions : dict
+        Dictionary with parameters names (`str`) as keys and distributions
+        or lists of parameters to try. Distributions must provide a ``rvs``
+        method for sampling (such as those from scipy.stats.distributions).
+        If a list is given, it is sampled uniformly.
+        If a list of dicts is given, first a dict is sampled uniformly, and
+        then a parameter is sampled using that dict as above.
+    n_iter : integer
+        Number of parameter settings that are produced.
+    random_state : int, default None
+        Pass an int for reproducible output across multiple
+        function calls.
+    """
+    def __init__(self, param_distributions, n_iter, random_state=None):
+
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.param_distributions = param_distributions
+
+    # ===============================================
+    # noinspection PyStatementEffect,PyUnresolvedReferences,PyProtectedMember,PyCallingNonCallable
+    def sample(self):
+        """
+        Perform the sampling from the data grid provided previously.
+
+        Returns
+        -------
+        param_combi : list of tuple
+            list of sampled parameter combination
+        """
+        self.param_distributions = self.param_distributions.copy()
+
+        for p_k, p_v in self.param_distributions.items():
+            self.param_distributions[p_k] = _check_param(p_v)
+
+        all_lists = all(not hasattr(p, "rvs")
+                        for p in self.param_distributions.values())
+
+        seed = (random.randint(1, 100) if self.random_state is None
+                else self.random_state + 1)
+        random.seed(seed)
+
+        if all_lists:
+            param_combi = list(product(*self.param_distributions.values()))
+            grid_size = len(param_combi)
+
+            if grid_size < self.n_iter:
+                raise ValueError(
+                    f"The total space of parameters {grid_size} is smaller "
+                    f"than n_iter={self.n_iter}. Try with KerasGridSearch.")
+            param_combi = random.sample(param_combi, self.n_iter)
+
+        else:
+            param_combi = []
+            k = self.n_iter
+            for i in range(self.n_iter):
+                dist = self.param_distributions
+                params = []
+                for j, v in enumerate(dist.values()):
+                    if hasattr(v, "rvs"):
+                        params.append(v.rvs(random_state=seed * (k + j)))
+                    else:
+                        params.append(v[random.randint(0, len(v) - 1)])
+                    k += i + j
+                param_combi.append(tuple(params))
+
+        # reset seed
+        np.random.mtrand._rand
+
+        return param_combi
--- chaitunkeras/search.py
+++ chaitunkeras/search.py
@@ -16,7 +16,7 @@ import pandas as pd
 
 from itertools import product
 from tensorflow.python.framework.errors_impl import ResourceExhaustedError
-from ._tools import _check_param
+from ._tools import _check_param, ParameterSampler
 
 # =================================================================================
 class KerasGridSearch(object):
@@ -51,7 +51,7 @@ class KerasGridSearch(object):
                validation_split=0.0,
                **fitargs):
         """
-        Performs a search across de parameters grid creating
+        Performs a search across the parameters grid creating
         all the possible trials and evaluating on the validation set provided.
 
         Parameters
@@ -74,6 +74,7 @@ class KerasGridSearch(object):
 
         if not isinstance(self.param_grid, dict):
             raise ValueError("Param_grid must be in dict format")
+        self.param_grid = self.param_grid.copy()
 
         for p_key, p_value in self.param_grid.items():
             self.param_grid[p_key] = _check_param(p_value)
@@ -171,29 +172,158 @@ class KerasGridSearch(object):
 
         return None
 # =================================================================================
-# class KerasRandomSearch(object):
-#     """
-#
-#     """
-#     def __init__(self,
-#                  hypermodel,
-#                  param_grid,
-#                  n_trials,
-#                  monitor='val_loss',
-#                  greater=False,
-#                  tuner_verbose=1):
-#
-#         self.hypermodel = hypermodel
-#         self.param_grid = param_grid
-#         self.n_trials = n_trials
-#         self.monitor = monitor
-#         self.greater = greater
-#         self.verbose = tuner_verbose
-#         self.trials = []
-#         self.scores = []
-#         self.best_params = None
-#         self.best_score = None
-#         self.best_model = None
-#         self.best_history = None
+class KerasRandomSearch(object):
+    """
+
+    """
+    def __init__(self,
+                 hypermodel,
+                 param_grid,
+                 n_trials,
+                 monitor='val_loss',
+                 greater=False,
+                 tuner_verbose=1):
+
+        self.hypermodel = hypermodel
+        self.param_grid = param_grid
+        self.n_trials = n_trials
+        self.monitor = monitor
+        self.greater = greater
+        self.verbose = tuner_verbose
+        self.trials = []
+        self.scores = []
+        self.best_params = None
+        self.best_score = None
+        self.best_model = None
+        self.best_history = None
 
 # ===============================================
+    def search(self,
+               x,
+               y=None,
+               validation_data=None,
+               validation_split=0.0,
+               **fitargs):
+        """
+        Performs a search across a random selection of the parameters of the grid, creating
+        a certain number of trials and evaluating on the validation set provided.
+
+        Parameters
+        ----------
+        x : multi types
+            Input data. All the input format supported by Keras model are accepted.
+        y : multi types, default None
+            Target data. All the target format supported by Keras model are accepted.
+        validation_data : multi types, default None
+            Data on which to evaluate the loss and any model metrics at the end of each epoch.
+            All the validation_data format supported by Keras model are accepted.
+        validation_split : float, default 0.0
+            Float between 0 and 1. Fraction of the training data to be used as validation data.
+        **fitargs :
+            Additional fitting arguments, the same accepted in Keras model.fit(...).
+        """
+        if validation_data is None and validation_split == 0.0:
+            raise ValueError("Must pass either validation data or a validation split")
+
+        if not isinstance(self.param_grid, dict):
+            raise ValueError("Param_grid must be in dict format")
+        self.param_grid = self.param_grid.copy()
+
+        max_trials = np.prod([len(p) for p in self.param_grid.values()])
+
+        start_score = -np.inf if self.greater else np.inf
+        self.best_score = start_score
+
+        eval_epoch = np.argmax if self.greater else np.argmin
+        eval_score = np.max if self.greater else np.min
+
+        if self.verbose == 1:
+            print(f"{self.n_trials} trials to do of {max_trials} options in the parameter grid")
+            verbose = fitargs['verbose'] if 'verbose' in fitargs.keys() else 1
+        else:
+            verbose = 0
+
+        fitargs['verbose'] = verbose
+        tunable_fitargs = ['batch_size', 'epochs', 'steps_per_epoch', 'class_weight']
+
+        rs = ParameterSampler(param_distributions=self.param_grid,
+                              n_iter=self.n_trials)
+
+        sampled_params = rs.sample()
+
+        for trial, param in enumerate(sampled_params):
+
+            param = dict(zip(self.param_grid.keys(), param))
+            model = self.hypermodel(param)
+
+            fit_param = {k: v for k, v in param.items() if k in tunable_fitargs}
+            all_fitargs = {**fitargs, **fit_param}
+
+            if self.verbose == 1:
+                print(f"===== Trial {trial + 1}/{self.n_trials} =====")
+
+            try:
+                history = model.fit(x=x, y=y,
+                                    validation_split=validation_split,
+                                    validation_data=validation_data,
+                                    **all_fitargs)
+            except ResourceExhaustedError as err:
+                print(f"Resource Exhausted Error: {err}")
+                continue
+
+            epoch = eval_epoch(history.history[self.monitor])
+            score = np.float32(history.history[self.monitor][epoch])
+            param['epochs'] = epoch + 1
+            param['score'] = score
+
+            best_score = eval_score([self.best_score, score])
+
+            if self.best_score != best_score:
+                self.best_params = param
+                self.best_model = model
+                self.best_history = history.history
+                self.best_score = best_score
+
+            self.trials.append(param)
+            self.scores.append(score)
+
+            if self.verbose == 1:
+                print(f"Score: {score} at epoch {epoch + 1}")
+
+        if self.verbose == 1:
+            print(f"---- Search completed ----\nBest score: {self.best_score}")
+
+        return None
+
+# ===============================================
+    def save_trials(self, path):
+        """
+        Save the trials params and score of the search method into a text file.
+
+        Parameters
+        ----------
+        path: str
+            Indicates the path of the text file.
+        """
+
+        if not self.trials:
+            print(f"No trials to save. Use the method 'search' before.")
+            return None
+
+        df = pd.DataFrame(self.trials)
+
+        if os.path.isfile(path):
+            cols = pd.read_csv(path, index_col=None, nrows=0, sep=' ').columns.tolist()
+
+            if sorted(df.columns.to_list()) == sorted(cols):
+                df[cols].to_csv(path, sep=' ', header=None, mode='a', index=False)
+            else:
+                new_cols = cols + [key for key in df.columns.to_list() if key not in cols]
+                new_df = pd.read_csv(path, index_col=None, sep=' ')
+                new_df = new_df.reindex(columns=new_cols)
+                new_df = new_df.append(df.reindex(columns=new_cols))
+                new_df.to_csv(path, sep=' ', index=False)
+        else:
+            df.to_csv(path, sep=' ', index=False)
+
+        return None
